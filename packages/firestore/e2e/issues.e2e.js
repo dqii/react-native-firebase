f9ce428de0c584499642d1020c0a1b17ab15c6d5
Mike Hardy <github@mikehardy.net>
2021-11-24 10:02:37 -0500

    test(firestore, types): check store/retrieve/query across SDKs
    validate that types go into and come out of firestore consistently
    using different SDKs
    
    This probes the problem from #3004

--- packages/firestore/e2e/issues.e2e.js
+++ packages/firestore/e2e/issues.e2e.js
@@ -16,9 +16,29 @@
  */
 
 const COLLECTION = 'firestore';
+const { getE2eEmulatorHost } = require('@react-native-firebase/app/e2e/helpers');
+const jsFirebase = require('firebase/compat/app');
+const { wipe } = require('./helpers');
+require('firebase/compat/firestore');
+
+const testTypes = {
+  stringType: 'stringtype',
+  numberType: 42,
+  doubleType: 3.14159,
+  booleanTypeTrue: true,
+  booleanTypeFalse: false,
+  // dateType: new Date(),
+  // mapType: { mapProperty1: 'mapProperty1', mapProperty2: 'mapProperty2' },
+  // arrayType: ['arrayEntry1', 'arrayEntry2'],
+  nullType: null,
+  // textType
+  // geoPointType
+  // referenceType
+  // bytesType
+};
 
 describe('firestore()', function () {
-  describe(COLLECTION, function () {
+  describe('pagination', function () {
     before(async function () {
       await Promise.all([
         firebase.firestore().doc(`${COLLECTION}/wbXwyLJheRfYXXWlY46j`).set({ index: 2, number: 2 }),
@@ -90,6 +110,88 @@ describe('firestore()', function () {
           doc.id.should.equal('mr7MdAygvuheF6AUtWma');
         }
       });
+    });
+  });
+
+  describe.only('type consistency', function () {
+    before(async function () {
+      // clear the database so we may assert correctly
+      await wipe();
+
+      // Put one example of each type in our document using JS SDK
+      jsFirebase.initializeApp(FirebaseHelpers.app.config());
+      jsFirebase.firestore().useEmulator(getE2eEmulatorHost(), 8080);
+      await jsFirebase.firestore().doc(`${COLLECTION}/dataTypeExamplesJS`).set(testTypes);
+
+      // Put one example of each type in our document using native
+      await firebase.firestore().doc(`${COLLECTION}/dataTypeExamplesNative`).set(testTypes);
+    });
+
+    it('types inserted by JS are equal when fetched native', async function () {
+      const ref = firebase.firestore().doc(`${COLLECTION}/dataTypeExamplesJS`);
+      const typesSnap = await ref.get();
+      const typesTestReturn = typesSnap.data();
+      // we do need to massage the Date just a little, it comes back as a Timestamp
+      // typesTestReturn.dateType = typesTestReturn.dateType.toDate();
+      console.error('got typesTest: ' + JSON.stringify(typesTestReturn));
+      Object.keys(typesTestReturn).forEach(type => {
+        if (typesTestReturn[type] === undefined) {
+          console.error('expected type was not defined: ' + type);
+          should.equal(typesTestReturn[type] === undefined, false);
+        }
+        // expect(typesTestReturn[type]).to.deep.equal(typesTypes[type]);
+        should.equal(typesTestReturn[type], testTypes[type]);
+      });
+    });
+
+    it('types inserted by native are equal to JS types when both fetched native', async function () {
+      let ref = firebase.firestore().doc(`${COLLECTION}/dataTypeExamplesJS`);
+      let typesSnap = await ref.get();
+      const typesTestReturnJS = typesSnap.data();
+      ref = firebase.firestore().doc(`${COLLECTION}/dataTypeExamplesNative`);
+      typesSnap = await ref.get();
+      const typesTestReturnNative = typesSnap.data();
+      // we do need to massage the Date just a little, it comes back as a Timestamp
+      // typesTestReturn.dateType = typesTestReturn.dateType.toDate();
+      console.error('got typesTestJS: ' + JSON.stringify(typesTestReturnJS));
+      Object.keys(typesTestReturnJS).forEach(type => {
+        if (typesTestReturnJS[type] === undefined) {
+          console.error('expected type was not defined: ' + type);
+          should.equal(typesTestReturnJS[type] === undefined, false);
+        }
+        // expect(typesTestReturnJS[type]).to.deep.equal(typesTypes[type]);
+        should.equal(typesTestReturnJS[type], testTypes[type]);
+      });
+
+      console.error('got typesTestNative: ' + JSON.stringify(typesTestReturnNative));
+      Object.keys(typesTestReturnNative).forEach(type => {
+        if (typesTestReturnNative[type] === undefined) {
+          console.error('expected type was not defined: ' + type);
+          should.equal(typesTestReturnNative[type] === undefined, false);
+        }
+        // expect(typesTestReturnNative[type]).to.deep.equal(typesTypes[type]);
+        should.equal(typesTestReturnNative[type], testTypes[type]);
+        should.equal(typesTestReturnNative[type], typesTestReturnJS[type]);
+      });
+    });
+
+    it('types inserted by JS may be queried by native with filters', async function () {
+      const ref = firebase
+        .firestore()
+        .collection(`${COLLECTION}`)
+        .where('numberType', 'in', [42, 43]);
+      typesSnap = await ref.get();
+      should.equal(typesSnap.docs.length, 2);
+      // typesSnap.docs.forEach((doc, i) => {
+      //   console.error(`got typesTestJS item ${doc.ref.path}: ${JSON.stringify(doc.data())}`);
+      // });
+      // Object.keys(typesTestReturnJS).forEach(type => {
+      //   if (typesTestReturnJS[type] === undefined) {
+      //     console.error('expected type was not defined: ' + type);
+      //     should.equal(typesTestReturnJS[type] === undefined, false);
+      //   }
+      //   should.equal(typesTestReturnJS[type], testTypes[type]);
+      // });
     });
   });
 });
